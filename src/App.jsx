9fafe0cceaded9d29eff0a1ffb85b2496657031e
Aswin Kumar <achu4798@gmail.com>
2023-12-07 20:59:51 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Aswin Kumar",
+  title: "Python Developer & AI/ML Engineer",
+  email: "achu4798@gmail.com",
+  gitHub: "https://github.com/AswinKumar1",
+  linkedIn: "https://www.linkedin.com/in/aswin-kumar-903b6b77/",
 };
 
 const primaryColor = "#4E567E";
